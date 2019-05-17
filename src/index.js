ba753c3cf6bf19d622584454b75279841d5739f2
Tony Tahmouch <ttahmouch@gmail.com>
2019-05-16 22:26:07 -0400

    TT | Extracting the `<App>` component usage into the entry point of the application, and creating declarative subcomponents for the `<RootView>` and its children.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
+/**
+ * TODO:
+ * + Universally support setting string `data-*` attributes whether or not the component was already rendered.
+ * + Eventually get everything to be pure XML instead of JSX.
+ * + Rename async to request?
+ */
 import ReactDOM from 'react-dom';
-import App from './declarative/view';
+import React from 'react';
+import {RootView} from './declarative/components';
+import states from './declarative/state';
+import styles from './declarative/style';
+import requests from './declarative/async';
+import {reducers} from './imperative/reducers';
+import {dispatchers} from './imperative/dispatchers';
+import {App} from './imperative/components';
 
-ReactDOM.render(App, document.getElementById("root"));
+ReactDOM.render(
+    (
+        <App states={states}
+             styles={styles}
+             requests={requests}
+             reducers={reducers}
+             dispatchers={dispatchers}
+             children={<RootView/>}/>
+    ),
+    document.getElementById('root')
+);
