b184cfd25f7d0d83f50c9535a913fdf0bf963e27
Tony Tahmouch <ttahmouch@gmail.com>
2019-05-22 14:07:38 -0400

    TT | Removing the logic for `comparators`, `declarators`, `interpolators`, and `selectors` from the `dispatchers` module because they were extracted into their own modules.

--- src/imperative/dispatchers/index.js
+++ src/imperative/dispatchers/index.js
@@ -4,118 +4,22 @@
  * Possibly treat request and response state the same with respect to comparing and dispatching selected state.
  */
 /* eslint-disable no-use-before-define,no-unused-vars */
-import jsonpath from 'jsonpath/jsonpath.min';
-import URITemplate from 'urijs/src/URITemplate';
-import Ajv from 'ajv';
 import {action, actions} from '../actions';
+import {compare, filterDeclaredResponsesMatchingResponse} from '../comparators';
+import {
+    toDeclarativeBody, toDeclarativeHeader, toDeclarativeHeaders, toDeclarativeResponse, toDeclarativeResponses,
+    toDeclarativeStatus
+} from '../declarators';
+import {interpolateJsTemplateUsingExpression, interpolateUriTemplate} from '../interpolators';
+import {selectState, selectStateUsingJsonPath} from '../selectors';
 import {asyncRequest} from '../request';
-import schema from '../../declarative/schema';
-
-const {anything_schema, any_positive_integer_schema} = schema;
 
 export const noop = (event) => console.log(event);
-
-export const mapChildrenToState = (children = [],
-                                   stateType = 'string',
-                                   state = '') => {
-    switch (stateType) {
-        case 'array':
-            return children
-                .filter(({name}) => name)
-                .map(({name, value}) => ({[name]: value}));
-        case 'dictionary':
-            return children
-                .filter(({name}) => name)
-                .reduce((map, {name, value}) => ({...map, [name]: [...map[name] || [], value]}), {});
-        case 'string':
-        default:
-            return state;
-    }
-};
 
 export const dispatch = (store = {dispatch: noop}, action = {}) => {
     console.log(action);
     return store.dispatch(action);
 };
-
-// Move to app?
-export const syncDispatcher = (type = '',
-                               store = {},
-                               dependencies = {
-                                   mapChildrenToState,
-                                   dispatch,
-                                   preventDefault: noop
-                               }) => {
-    const {mapChildrenToState, dispatch, preventDefault} = dependencies;
-
-    return (event = {preventDefault}) => {
-        const {target} = event;
-        const {
-            value: stateValue = '',
-            dataset: {stateType = 'string'}
-        } = target;
-        const children = Array.from(target);
-        const value = mapChildrenToState(children, stateType, stateValue);
-
-        event.preventDefault();
-
-        return dispatch(store, {type, value});
-    };
-};
-
-export const templateExpression = /{([^{}]*)}/g;
-
-export const interpolateJsTemplate = (template = '', params = {}, dependencies = {templateExpression}) => {
-    const {templateExpression} = dependencies;
-
-    return template.replace(templateExpression, (match, param) => {
-        const value = params[param];
-
-        return typeof value === 'string' ? value : match;
-    });
-};
-
-export const selectStateUsingJsonPath = (state = {}, selector = '', dependencies = {jsonpath}) => {
-    const {jsonpath} = dependencies;
-
-    return selector ? jsonpath.value(state, selector) : state;
-};
-
-export const selectStateUsingRegExp = (state = '', selector = '', regexp = new RegExp(selector)) => {
-    return (regexp.exec(state) || []).slice(1);
-};
-
-export const selectState = (state = {},
-                            selector = '',
-                            {
-                                regexp = undefined,
-                                schema = undefined
-                            },
-                            dependencies = {selectStateUsingJsonPath, selectStateUsingRegExp}) => {
-    const {selectStateUsingJsonPath, selectStateUsingRegExp} = dependencies;
-
-    if (regexp !== undefined) {
-        return selectStateUsingJsonPath(selectStateUsingRegExp(state, regexp), selector);
-    }
-
-    if (schema !== undefined) {
-        return selectStateUsingJsonPath(state, selector);
-    }
-
-    return state;
-};
-
-export const interpolateUriTemplate = (template = '', params = {}, dependencies = {URITemplate}) => {
-    const {URITemplate} = dependencies;
-
-    return new URITemplate(template).expand(params);
-};
-
-export const compareLiteral = (declaredLiteral = 0, value = 0) => declaredLiteral === value;
-
-export const compareRegexp = (declaredRegexp = '', value = '', regexp = new RegExp(declaredRegexp)) => regexp.test(value);
-
-export const compareSchema = (declaredSchema = {}, value = {}, schema = new Ajv()) => schema.validate(declaredSchema, value);
 
 export const dispatchDeclaredActionsWithSelectedState = (declaredActions = {
                                                              '$regexp_comparison': undefined,
@@ -146,25 +50,7 @@ export const dispatchDeclaredActionsWithSelectedState = (declaredActions = {
     return appStateActions.forEach(dispatchAction);
 };
 
-export const compare = ({
-                            '$literal_comparison': literal = undefined,
-                            '$regexp_comparison': regexp = undefined,
-                            '$schema_comparison': schema = undefined
-                        },
-                        value = '',
-                        dependencies = {compareLiteral, compareRegexp, compareSchema}) => {
-    const {compareLiteral, compareRegexp, compareSchema} = dependencies;
-
-    if (regexp !== undefined) {
-        return compareRegexp(regexp, value);
-    }
-
-    if (schema !== undefined) {
-        return compareSchema(schema, value);
-    }
-
-    return compareLiteral(literal, value);
-};
+//
 
 export const toComparableDeclarative = (declaredComparable = {}, dependencies = {compare}) => {
     const {compare} = dependencies;
@@ -186,59 +72,6 @@ export const toDispatchableDeclarative = (declaredComparable = {},
     };
 };
 
-export const toDeclarativeStatus = (declaredStatus = {'$schema_comparison': any_positive_integer_schema}) => {
-    return typeof declaredStatus !== 'number' ? declaredStatus : {'$literal_comparison': declaredStatus};
-};
-
-export const toDeclarativeHeader = (declaredHeader = '') => {
-    return typeof declaredHeader !== 'string' ? declaredHeader : {'$regexp_comparison': declaredHeader};
-};
-
-export const toDeclarativeHeaders = (declaredHeaders = {}, dependencies = {toDeclarativeHeader}) => {
-    const {toDeclarativeHeader} = dependencies;
-    const toHeadersArray = (header) => [header, declaredHeaders[header]];
-    const toHeadersObject = (headers, [header, value]) => ({...headers, [header]: toDeclarativeHeader(value)});
-
-    return Object.keys(declaredHeaders).map(toHeadersArray).reduce(toHeadersObject, {});
-};
-
-export const toDeclarativeBody = (declaredBody = {'$schema_comparison': anything_schema}) => {
-    return typeof declaredBody['$schema'] !== 'string' ? declaredBody : {'$schema_comparison': declaredBody};
-};
-
-export const toDeclarativeResponse = ({
-                                          '$status': declaredStatus = undefined,
-                                          '$headers': declaredHeaders = undefined,
-                                          '$body': declaredBody = undefined
-                                      },
-                                      dependencies = {
-                                          toDeclarativeStatus,
-                                          toDeclarativeHeader,
-                                          toDeclarativeHeaders,
-                                          toDeclarativeBody
-                                      }) => {
-    const {toDeclarativeStatus, toDeclarativeHeaders, toDeclarativeBody} = dependencies;
-
-    return {
-        '$status': toDeclarativeStatus(declaredStatus),
-        '$headers': toDeclarativeHeaders(declaredHeaders, dependencies),
-        '$body': toDeclarativeBody(declaredBody)
-    };
-};
-
-export const toDeclarativeResponses = (declaredResponses = [],
-                                       dependencies = {
-                                           toDeclarativeStatus,
-                                           toDeclarativeHeader,
-                                           toDeclarativeHeaders,
-                                           toDeclarativeBody,
-                                           toDeclarativeResponse
-                                       }) => {
-    const {toDeclarativeResponse} = dependencies;
-
-    return declaredResponses.map((declaredResponse) => toDeclarativeResponse(declaredResponse, dependencies));
-};
-
 export const toDecoratedDeclarative = (declarative = {},
                                        store = {},
                                        dependencies = {toDispatchableDeclarative, toComparableDeclarative}) => {
@@ -253,12 +86,12 @@ export const reviver = (key = '',
                         appState = {},
                         viewState = {},
                         dependencies = {
-                            selectStateUsingJsonPath, interpolateUriTemplate, interpolateJsTemplate,
+                            selectStateUsingJsonPath, interpolateUriTemplate, interpolateJsTemplateUsingExpression,
                             toDeclarativeStatus, toDeclarativeHeader, toDeclarativeHeaders, toDeclarativeBody,
                             toDeclarativeResponse, toDeclarativeResponses, toDecoratedDeclarative
                         }) => {
     const {
-        selectStateUsingJsonPath, interpolateUriTemplate, interpolateJsTemplate, toDeclarativeStatus,
+        selectStateUsingJsonPath, interpolateUriTemplate, interpolateJsTemplateUsingExpression, toDeclarativeStatus,
         toDeclarativeHeader, toDeclarativeBody, toDeclarativeResponses, toDecoratedDeclarative
     } = dependencies;
     const type = value === null ? 'null' : typeof value;
@@ -290,33 +123,14 @@ export const reviver = (key = '',
             return interpolateUriTemplate(uriTemplate, value);
         }
         if (jsTemplate) {
-            return interpolateJsTemplate(jsTemplate, value);
+            return interpolateJsTemplateUsingExpression(jsTemplate, value);
         }
     }
 
     return value;
 };
 
-export const compareHeaders = (declaredHeaders = {}, headers = {}, dependencies = {noop}) => {
-    const {noop} = dependencies;
-    const declaredHeaderNames = Object.keys(declaredHeaders);
-    const onHeader = ({header = '', compare = noop}) => compare(headers[header]);
-    const toDeclaredHeader = (header = '') => ({header, ...declaredHeaders[header]});
-
-    return declaredHeaderNames.length === declaredHeaderNames.map(toDeclaredHeader).filter(onHeader).length;
-};
-
-export const filterDeclaredResponsesMatchingResponse = (declaredResponses = [],
-                                                        response = {status: 0, headers: {}, body: {}},
-                                                        dependencies = {compareHeaders, noop}) => {
-    const {compareHeaders, noop} = dependencies;
-    const {status = 0, headers = {}, body = {}} = response;
-    const onStatus = ({'$status': {compare = noop}}) => compare(status);
-    const onHeaders = ({'$headers': declaredHeaders = {}}) => compareHeaders(declaredHeaders, headers);
-    const onBody = ({'$body': {compare = noop}}) => compare(body);
-
-    return declaredResponses.filter(onStatus).filter(onHeaders).filter(onBody);
-};
+//
 
 export const dispatchHeaders = (declaredHeaders = {}, headers = {}, dependencies = {noop}) => {
     const {noop} = dependencies;
@@ -383,6 +197,42 @@ export const dispatchResponse = (store = {},
     return dispatch(store, {type, request, event, response});
 };
 
+export const mapChildrenToState = (children = [], stateType = 'string', state = '') => {
+    switch (stateType) {
+        case 'array':
+            return children
+                .filter(({name}) => name)
+                .map(({name, value}) => ({[name]: value}));
+        case 'dictionary':
+            return children
+                .filter(({name}) => name)
+                .reduce((map, {name, value}) => ({...map, [name]: [...map[name] || [], value]}), {});
+        case 'string':
+        default:
+            return state;
+    }
+};
+
+// Move to app?
+export const syncDispatcher = (type = '', store = {},
+                               dependencies = {mapChildrenToState, dispatch, preventDefault: noop}) => {
+    const {mapChildrenToState, dispatch, preventDefault} = dependencies;
+
+    return (event = {preventDefault}) => {
+        const {target} = event;
+        const {value: stateValue = '', dataset: {stateType = 'string'}} = target;
+        const children = Array.from(target);
+        const value = mapChildrenToState(children, stateType, stateValue);
+        const action = {type, value};
+
+        event.preventDefault();
+
+        console.log(action);
+
+        return dispatch(store, action);
+    };
+};
+
 export const asyncDispatcher = (type = '',
                                 store = {getState: noop},
                                 dependencies = {
@@ -399,18 +249,18 @@ export const asyncDispatcher = (type = '',
         const {target} = event;
         const {
             value: stateValue = '',
-            dataset: {stateType = 'string', request: metadata = '{}'}
+            dataset: {stateType = 'string', request: declaredRequest = '{}'}
         } = target;
         const children = Array.from(target);
         const appState = store.getState();
         const viewState = mapChildrenToState(children, stateType, stateValue);
-        const request = JSON.parse(metadata, (key, value) => reviver(key, value, store, appState, viewState));
+        const request = JSON.parse(declaredRequest, (key, value) => reviver(key, value, store, appState, viewState));
         const onResponse = (request, event, response) => dispatchResponse(store, {type, request, event, response});
 
         event.preventDefault();
         dispatch(store, {type, request});
 
-        console.log(JSON.stringify(JSON.parse(metadata), null, 4));
+        console.log(JSON.stringify(JSON.parse(declaredRequest), null, 4));
         console.log(JSON.stringify(request, null, 4));
 
         return asyncRequest(request, onResponse);
