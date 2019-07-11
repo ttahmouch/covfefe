783b2cabc2b9f30f1481a2760909aee696340d1b
Tony Tahmouch <ttahmouch@gmail.com>
2019-07-10 21:36:32 -0400

    TT | Formatted syntax to make the components easier to read.

--- src/declarative/components/index.js
+++ src/declarative/components/index.js
@@ -11,53 +11,65 @@ export const RootView = (props) => (
 );
 
 export const Header = (props) => (
-    <nav data-style='navigation_bar'>
-        <Link to='/back'>
-            <div data-style='navigation_bar_back_button'>⬅</div>
+    <nav data-style="navigation_bar">
+        <Link to="/back">
+            <div data-style="navigation_bar_back_button">
+                ⬅
+            </div>
         </Link>
-        <Link to='/'>
-            <div data-style='navigation_bar_back_button'>◼︎</div>
+        <Link to="/">
+            <div data-style="navigation_bar_back_button">
+                ◼︎
+            </div>
         </Link>
-        <div data-style='navigation_bar_title' data-state='page_title'/>
+        <div data-style="navigation_bar_title"
+             data-state="page_title"/>
     </nav>
 );
 
 export const Body = (props) => (
     <>
-        <Route path='/back' exact component={IndexContent}/>
-        <Route path='/' exact component={BackContent}/>
+        <Route path="/back" exact component={IndexContent}/>
+        <Route path="/" exact component={BackContent}/>
     </>
 );
 
 export const IndexContent = (props) => (
-    <main data-style='main_content'>
-        <div data-style='card'>
-            <header data-style='card_header' data-state='card_title'/>
-            <div data-style='card_main_content'/>
+    <main data-style="main_content">
+        <div data-style="card">
+            <header data-style="card_header"
+                    data-state="card_title"/>
+            <div data-style="card_main_content"/>
         </div>
     </main>
 );
 
 export const BackContent = (props) => (
-    <main data-style='main_content'>
-        <div data-style='card'>
-            <header data-style='card_header' data-state='card_title_2'/>
-            <div data-style='card_main_content'/>
+    <main data-style="main_content">
+        <div data-style="card">
+            <header data-style="card_header"
+                    data-state="card_title_2"/>
+            <div data-style="card_main_content"/>
         </div>
     </main>
 );
 
 export const Footer = (props) => (
     <div>
-        <Json data-state='titles_array' data-state-prop='json'/>
-        <form data-action-prop='onSubmit' data-action='async_read'
-              data-state='titles_array' data-state-type='dictionary'>
+        <Json data-state="titles_dictionary"
+              data-state-prop="json"/>
+        <form data-action-prop="onSubmit"
+              data-action="async_read"
+              data-state="titles_dictionary"
+              data-state-type="dictionary">
             <div>
                 <label>Title:</label>
-                <input name='title'/>
+                <input name="title"/>
             </div>
             <div>
-                <button type='submit'>Read Titles Dictionary Async</button>
+                <button type="submit">
+                    Read Titles Dictionary Async
+                </button>
             </div>
         </form>
     </div>
