28de152c4073c1e5d0970db11b55caa4e5ac8599
Tony Tahmouch <ttahmouch@gmail.com>
2019-09-09 21:32:24 -0400

    TT | Support the new format of declarative selectors, e.g., `{'$selector': '', '$from': 'response'}`, and functional selectors, e.g., `({app, view, response}) => `.

--- src/imperative/selectors/index.js
+++ src/imperative/selectors/index.js
@@ -9,22 +9,56 @@ export const selectStateUsingRegExp = (selector = '',
                                        state = '',
                                        regexp = new RegExp(selector)) => (regexp.exec(state) || []).slice(1);
 
-// Schema is not necessary.
-export const selectState = ({
-                                '$regexp_comparison': regexp = undefined,
-                                '$schema_comparison': schema = undefined,
-                                '$from_app_state': appStateSelector = '',
-                                '$from_view_state': viewStateSelector = '',
-                                '$from_response_state': responseStateSelector = '',
-                                '$selector': $selector = appStateSelector || viewStateSelector || responseStateSelector || ''
-                            },
-                            state = {},
-                            dependencies = {selectStateUsingJsonPath, selectStateUsingRegExp}) => {
-    const {selectStateUsingJsonPath, selectStateUsingRegExp} = dependencies;
+/**
+ * Remember to solve the issue of name collision of renaming $schema_comparison to $schema.
+ * $compare: {$regexp,$schema,$literal}
+ */
+export const selectState = (selector = {'$selector': '', '$from': 'response'},
+                            state = {'app': {}, 'view': {}, 'response': {}},
+                            dependencies = {selectStateUsingJsonPath}) => {
+    const {selectStateUsingJsonPath} = dependencies;
 
-    if (regexp !== undefined) {
-        return selectStateUsingJsonPath($selector, selectStateUsingRegExp(regexp, state));
+    if (typeof selector === 'function') {
+        // I completely fucking forgot that functions are being parsed from a JSON string meaning that's why they won't
+        // be called. They won't exist. Functional selectors may have to be replaced with a filename or be serialized
+        // and evaluated which sounds horrible, e.g., {$file_path, $json_path}
+        // I may also be able to deserialize the entire request object at the proper time before dispatching an async
+        // action as opposed to serializing the function or requiring it from a separate file.
+        return selector(state);
     }
 
-    return selectStateUsingJsonPath($selector, state);
+    if (typeof selector === 'object' && selector !== null) {
+        const {
+            // $file = '',
+            // $function = $file ? require($file) : undefined,
+            $selector = '',
+            $from = 'response',
+            // $state = state[$from] || {},
+            $state = state[$from],
+            // $selected = $function ? $function($state) : selectStateUsingJsonPath($selector, $state)
+            $selected = selectStateUsingJsonPath($selector, $state)
+        } = selector;
+
+        // return select ? selected : selector;
+        return $selected;
+    }
+
+    return selector;
+};
+
+export const captureAndSelectState = (captor = {'$regexp': '', '$from': 'response'},
+                                      selector = {'$selector': '', '$from': 'response'},
+                                      state = {'app': {}, 'view': {}, 'response': {}},
+                                      dependencies = {selectStateUsingRegExp, selectState}) => {
+    const {selectStateUsingRegExp, selectState} = dependencies;
+    const {
+        $regexp = '',
+        $from = 'response',
+        // $state = state[$from] || {},
+        $state = state[$from],
+        // Shift the ternary into selectStateUsingRegExp?
+        $captured = $regexp ? selectStateUsingRegExp($regexp, $state) : $state
+    } = captor;
+
+    return selectState(selector, {...state, [$from]: $captured});
 };
