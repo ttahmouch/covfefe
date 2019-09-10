187cb89a4c46461aedcad5533d3c86d16606c6ef
Tony Tahmouch <ttahmouch@gmail.com>
2019-09-10 00:21:48 -0400

    TT | Supporting the new declarative syntax for HTTP transactions in the declaration file.

--- src/declarative/async/index.js
+++ src/declarative/async/index.js
@@ -1,7 +1,3 @@
-import schema from "../schema";
-
-const {titles_dictionary_schema, json_content_type_schema} = schema;
-
 /**
  * TODO:
  * + Document `$uri_template` and `$js_template` TEMPLATE expressions.
@@ -9,6 +5,8 @@ const {titles_dictionary_schema, json_content_type_schema} = schema;
  * + Document `$literal_comparison`, `$schema_comparison`, `$regexp_comparison` COMPARISON expressions.
  * + Document `$to_app_state` DISPATCH expressions.
  * + Allow JSON Schemas to be referenced by ID, i.e., no need to import `schema`.
+ * Start thinking about the structures in the JSON as types based on the properties they retain.
+ * Add synchronous actions to this JSON structure by just adding `{$actions:[]}`.
  */
 export default {
     "ASYNC_READ_TITLES_DICTIONARY": {
@@ -18,64 +16,137 @@ export default {
             path: "titlesDictionary",
             extension: ".json"
         },
+        // $uri: {$template: "/{path}{extension}", path: "titlesDictionary", extension: ".json"},
         $headers: {
             "custom": {
                 $js_template: "{value1}{value2}",
-                value1: {
-                    $from_view_state: "$.title[0]"
-                },
+                // Support functional selection.
+                value1: {$selector: "$.title[0]", $from: "view"},
                 value2: {
-                    $from_app_state: "$.title"
+                    $js_template: "{value1}{value2}",
+                    value1: {$selector: "$.title[0]", $from: "view"},
+                    value2: {$selector: "$.title", $from: "app"}
                 }
-                // $value2: {$from_app_state: ({titles_array = []}) => titles_array[0] || ""}
             }
         },
-        $to_app_state: [{
-            $action: "update",
-            $state: "title"
-        }],
+        $actions: [
+            {
+                $action: "update",
+                $state: "loading",
+                $value: true
+            },
+            {
+                $action: "update",
+                $state: "title",
+                $select: {$selector: "$.title", $from: "app"}
+            },
+            {
+                $action: "update",
+                $state: "title",
+                $select: {$selector: "$.title[0]", $from: "view"}
+            },
+            {
+                $action: "update",
+                $state: "loading",
+                $select: () => true
+            },
+            {
+                $action: "update",
+                $state: "title",
+                $select: ({app = {title: ''}}) => app.title
+            },
+            {
+                $action: "update",
+                $state: "title",
+                $select: ({view = {title: ['']}}) => view.title[0]
+            }
+        ],
         $responses: [
             {
                 $status: {
-                    $literal_comparison: 200,
-                    $to_app_state: [{
-                        $action: "update",
-                        $state: "status"
-                    }]
+                    $literal: 200,
+                    $actions: [
+                        {
+                            $action: "update",
+                            $state: "status"
+                        },
+                        {
+                            $action: "update",
+                            $state: "status",
+                            $select: ({response = 200}) => response
+                        }
+                    ]
                 },
                 $headers: {
                     "content-type": {
-                        $schema_comparison: json_content_type_schema,
-                        $to_app_state: [{
-                            $action: "update",
-                            $state: "header"
-                        }]
+                        $schema_comparison: {
+                            "$schema": "http://json-schema.org/schema#",
+                            "type": "string",
+                            "pattern": "application[/]json"
+                        },
+                        $actions: [
+                            {
+                                $action: "update",
+                                $state: "header"
+                            },
+                            {
+                                $action: "update",
+                                $state: "header",
+                                $select: ({response = 'application/json'}) => response
+                            }
+                        ]
                     },
                     "content-length": {
-                        $regexp_comparison: "^([0-9]+)$",
-                        $to_app_state: [{
-                            $action: "update",
-                            $state: "length",
-                            $from_response_state: "$.0"
-                        }]
+                        $regexp: "^([0-9]+)$",
+                        $actions: [
+                            {
+                                $action: "update",
+                                $state: "length",
+                                $select: {$selector: "$.0", $from: "response"}
+                            },
+                            {
+                                $action: "update",
+                                $state: "length",
+                                $select: ({response = ['0']}) => response[0]
+                            }
+                        ]
                     }
                 },
                 $body: {
-                    $schema_comparison: titles_dictionary_schema,
-                    $to_app_state: [
+                    $schema_comparison: {
+                        "$schema": "http://json-schema.org/schema#",
+                        "type": "object",
+                        "properties": {"titles": {"type": "array"}}
+                    },
+                    $actions: [
+                        {
+                            $action: "update",
+                            $state: "titles_dictionary"
+                        },
                         {
                             $action: "update",
                             $state: "title",
-                            $from_response_state: "$.titles.0.title"
+                            $select: {$selector: "$.titles.0.title", $from: "response"}
                         },
                         {
                             $action: "update",
                             $state: "titles_array",
-                            $from_response_state: "$.titles"
+                            $select: {$selector: "$.titles", $from: "response"}
                         },
                         {
                             $action: "update",
-                            $state: "titles_dictionary"
+                            $state: "titles_dictionary",
+                            $select: ({response = {titles: []}}) => response
+                        },
+                        {
+                            $action: "update",
+                            $state: "title",
+                            $select: ({response = {titles: [{title: ''}]}}) => response.titles[0].title
+                        },
+                        {
+                            $action: "update",
+                            $state: "titles_array",
+                            $select: ({response = {titles: []}}) => response.titles
                         }
                     ]
                 }
@@ -86,33 +157,37 @@ export default {
         // Revise these to use $ prefix.
         $method: "GET",
         $uri: "/titlesDictionary.json",
-        $to_app_state: [{
-            $action: "update",
-            $state: "title"
-        }],
         $responses: [
             {
                 $status: 200,
-                $headers: {
-                    "content-type": "application[/]json"
-                },
-                $body: titles_dictionary_schema
+                $headers: {"content-type": "application[/]json"},
+                $body: {
+                    "$schema": "http://json-schema.org/schema#",
+                    "type": "object",
+                    "properties": {"titles": {"type": "array"}}
+                }
             },
             {
-                // literal
                 $status: 200,
-                // regex
-                $headers: {
-                    "content-type": "application[/]json"
-                },
-                // schema
+                $headers: {"content-type": "application[/]json"},
                 $body: {
-                    $schema_comparison: titles_dictionary_schema,
-                    $to_app_state: [{
-                        $action: "update",
-                        $state: "titles_array",
-                        $from_response_state: "$.titles"
-                    }]
+                    $schema_comparison: {
+                        "$schema": "http://json-schema.org/schema#",
+                        "type": "object",
+                        "properties": {"titles": {"type": "array"}}
+                    },
+                    $actions: [
+                        {
+                            $action: "update",
+                            $state: "titles_array",
+                            $select: {$selector: "$.titles", $from: "response"}
+                        },
+                        {
+                            $action: "update",
+                            $state: "titles_array",
+                            $select: ({response = {titles: []}}) => response.titles
+                        }
+                    ]
                 }
             }
         ]
