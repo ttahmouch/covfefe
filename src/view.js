79f0f28f8c237e85ca3dd3cd30a587c19fd7fbe8
Tony Tahmouch <ttahmouch@gmail.com>
2019-10-12 21:52:48 -0400

    TT | Adding the entry point view in various formats, i.e., JS, JSON, and XML.

---
+++ src/view.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-use-before-define */
+import React from "react";
+
+export default <div data-view="root"/>;
+/**
+ "Support remote JSON and XML variations of JSX from remote resources using actions like normal state,
+ e.g., data-action='read' data-state='view'.",
+ */
