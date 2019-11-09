91ef7e26dc221cfa004a8d8caab326169e6ef135
Tony Tahmouch <ttahmouch@gmail.com>
2019-11-09 00:46:56 -0500

    TT | Initial the `App` component from the application manifest object, and initialize React.createElement right outside the `App` component creation.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,39 @@
  * + Universally support setting string `data-*` attributes whether or not the component was already rendered.
  * + Eventually get everything to be pure XML instead of JSX.
  * + Rename async to request?
+ * + Allow defining initial application state using JSON Schema.
  */
 import ReactDOM from 'react-dom';
-import React from 'react';
-import {RootView} from './declarative/components';
-import states from './declarative/state';
-import styles from './declarative/style';
-import requests from './declarative/async';
-import {reducers} from './imperative/reducers';
-import {dispatchers} from './imperative/dispatchers';
-import {App} from './imperative/components';
+import React, {createElement} from 'react';
+import app from './app';
+import {App, reactMethodWithCustomDataProps} from './imperative/components';
 
-const children = <RootView/>;
+React.createElement = reactMethodWithCustomDataProps({createElement}, app);
 
-ReactDOM.render(
-    <App states={states}
-         styles={styles}
-         requests={requests}
-         reducers={reducers}
-         dispatchers={dispatchers}
-         children={children}/>,
-    document.getElementById('root')
-);
+const element = <App app={app}/>;
+const root = document.getElementById('root');
+
+ReactDOM.render(element, root);
+
+console.group('Application Element:');
+console.log(element);
+console.groupEnd();
+
+// import ReactDOMServer from 'react-dom/server';
+// const responseXml = ReactDOMServer.renderToString(
+//     React.createElement(
+//         'View',
+//         {},
+//         React.createElement(
+//             'FlatList',
+//             {
+//                 'data-state': 'array',
+//                 'data-item': 'view',
+//                 'data':{display:'block'}
+//             }
+//         )
+//     )
+// );
+// console.log(responseXml);
+// const parsedXml = new DOMParser().parseFromString(responseXml, 'application/xhtml+xml');
+// console.log(parsedXml);
