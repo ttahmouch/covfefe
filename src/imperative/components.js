99d06022d08611e0ea7ce45b64396cc54195331b
Tony Tahmouch <ttahmouch@gmail.com>
2019-11-09 00:44:58 -0500

    TT | Introduce `data-view` prop to allow binding views to existing elements instead of state, e.g., server-side rendered views.

--- src/imperative/components.js
+++ src/imperative/components.js
@@ -2,14 +2,13 @@
 /**
  * TODO:
  * + Extract most logic of App into functions.
- * + Consolidate createElement and cloneElement logic for custom data-* props.
  * The dispatchers need to be regenerated every time the state changes because there will be no way to CRUD new state otherwise.
  * Refactor dispatchers() to rely on retrieving the most current state from the store since this is initial state.
  * Am I thinking about this part wrong? Should root state keys never get added at runtime? Should the nest structure of the state keys change instead?
  * That would mean that new reducers and dispatchers wouldn't need to be created dynamically.
  * I probably also need to finally add PATCH support to the reducers and dispatchers.
  */
-import React, {Component} from 'react';
+import React, {Component, createElement, Fragment, isValidElement} from 'react';
 import {combineReducers, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {action} from './actions';
@@ -22,29 +21,64 @@ export const getDispatcher = (dispatchers = {}, action = '') => dispatchers[acti
 
 export const getState = (states = {}, state = '') => states[state];
 
+export const getView = (views = {}, view = '') => views[view];
+
 export const getRequest = (requests = {}, request = '') => requests[request];
 
+// Support data-platform and data-view and data-compare=comparator with data-state|view|style=selector.
+// Support view stacks and stack selection using a selector.
+// Support custom data structure operations in the reducers.
+// Support data-action=crud data-state|view|style=selector data-event=onRender
+
 export const mapCustomPropsToReactProps = (props = {},
-                                           styles = {},
-                                           store = {getState: getEmptyObject},
-                                           dispatchers = {},
-                                           dependencies = {getStyle, getState, getDispatcher, action}) => {
-    const {getStyle, getState, getDispatcher, action} = dependencies;
+                                           app = {
+                                               $store: {getState: getEmptyObject},
+                                               $styles: {},
+                                               $dispatchers: {},
+                                           },
+                                           dependencies = {
+                                               getStyle,
+                                               getState,
+                                               getDispatcher,
+                                               action,
+                                           }) => {
+    const {
+        getStyle,
+        getState,
+        getDispatcher,
+        action,
+    } = dependencies;
+    const {
+        $store = {getState: getEmptyObject},
+        $styles = {},
+        $dispatchers = {},
+    } = app;
+    const $states = $store.getState();
+    // data-view-inject vs data-view-replace; Support both? Probably keep only one way.
+    // Dispatchers aren't working right now.
+    // Extract selectors out into the selectors.js(on).
+    // Make actions determine if they are async or sync based on their declared type or duck type as opposed to their name.
+    // Make reducers support generic data structure operations such as push, pop, insert, etc.
     const {
         style,
         'data-style': $style = '',
         'data-state': $state = '',
-        'data-state-prop': $stateProp = 'children',
         'data-action': $action = '',
-        'data-action-prop': $actionProp = 'data-action-prop',
+        'data-dispatcher': $dispatcher = action($action, $state),
+        'data-event': $event = 'data-event',
+        'data-style-value': $styleValue = getStyle($styles, $style),
+        'data-state-value': $stateValue = getState($states, $state),
+        'data-dispatcher-value': $dispatcherValue = getDispatcher($dispatchers, $dispatcher),
+        'data-bind-style': $bindStyle = $style ? 'style' : 'data-bind-style',
+        'data-bind-state': $bindState = $state ? 'children' : 'data-state',
         'data-action-request': $actionRequest = action($action, $state),
     } = props;
 
     return {
         ...props,
-        style: style || getStyle(styles, $style),
-        [$stateProp]: getState(store.getState(), $state),
-        [$actionProp]: getDispatcher(dispatchers, action($action, $state)),
+        [$bindStyle]: style || $styleValue,
+        [$bindState]: $stateValue,
+        [$event]: $dispatcherValue,
         'data-action-request': $actionRequest,
     };
 };
@@ -57,34 +91,74 @@ export const areDataProps = (props = {}, dependencies = {isDataProp}) => {
     return props !== null && typeof props === 'object' && Object.keys(props).filter(isDataProp).length;
 };
 
-export const toReactProps = (props = {}, styles = {}, store = {}, dispatchers = {},
+export const toReactProps = (props = {},
+                             app = {},
                              dependencies = {areDataProps, mapCustomPropsToReactProps}) => {
     const {areDataProps, mapCustomPropsToReactProps} = dependencies;
 
-    return areDataProps(props) ? mapCustomPropsToReactProps(props, styles, store, dispatchers) : props;
+    return areDataProps(props) ? mapCustomPropsToReactProps(props, app) : props;
 };
 
-export const reactMethodWithCustomDataProps = (method = React.createElement, styles = {}, store = {},
-                                               dispatchers = {}, dependencies = {toReactProps}) => {
-    const {toReactProps} = dependencies;
-    const fromCustomDataPropsToReactProps = (props) => toReactProps(props, styles, store, dispatchers);
+export const isFragment = (fragment = Fragment) => fragment === Fragment;
 
-    return (...args) => method.apply(this, Array.from(args).map(fromCustomDataPropsToReactProps));
+export const isComponent = (component = Component) => typeof component === 'function';
+
+export const isElementLike = (element = {type: '', props: {children: []}}) => (
+    typeof element === 'object'
+    && element !== null
+    && typeof element.$$typeof === 'undefined'
+);
+
+export const isElement = (element = {}, dependencies = {isElementLike, isValidElement}) => {
+    const {isElementLike, isValidElement} = dependencies;
+
+    return isElementLike(element) || isValidElement(element);
 };
 
-export const toClonedElement = (element = {}) => {
-    const {props = {}} = element;
-    const {children = []} = props;
+export const getType = (components = {},
+                        type = '',
+                        dependencies = {isFragment, isComponent, isElement}) => {
+    const {isFragment, isComponent, isElement} = dependencies;
+    const $components = {...components, '': Fragment, Fragment};
 
-    return React.cloneElement(element, props, React.Children.map(children, toClonedElement));
+    return isElement(type) || isFragment(type) || isComponent(type)
+        ? type
+        : $components[type] || type || Fragment;
 };
 
-export const proxyReactMethodsToSupportCustomDataProps = (styles = {}, store = {}, dispatchers = {},
-                                                          dependencies = {reactMethodWithCustomDataProps}) => {
-    const {reactMethodWithCustomDataProps} = dependencies;
+export const reactMethodWithCustomDataProps = (method = {createElement},
+                                               app = {$views: {}, $components: {}},
+                                               dependencies = {
+                                                   toReactProps,
+                                                   getType,
+                                                   getView,
+                                                   isElement
+                                               }) => {
+    const {
+        toReactProps,
+        getType,
+        getView,
+        isElement
+    } = dependencies;
+    const {createElement} = method;
+    const toChild = (child) => typeof child === 'string' ? child : toElement(child);
+    const toElement = (type = '', props = {}, ...children) => {
+        const {$views = {}, $components = {}} = app;
+        const {'data-view': $selector = '', ...$props} = props;
+        const $view = getView($views, $selector);
+        const $element = $view || type;
 
-    React.createElement = reactMethodWithCustomDataProps(React.createElement, styles, store, dispatchers);
-    React.cloneElement = reactMethodWithCustomDataProps(React.cloneElement, styles, store, dispatchers);
+        if (isElement($element)) {
+            const {type = '', props: {children = [], ...props} = {}} = $element;
+            const $type = getType($components, type);
+            const $children = [].concat(children).map(toChild);
+            return toElement($type, {...props, ...$props}, ...$children);
+        }
+
+        return createElement(type, toReactProps(props, app), ...children);
+    };
+
+    return toElement;
 };
 
 export class App extends Component {
@@ -92,24 +166,29 @@ export class App extends Component {
         super(props);
 
         const {
-            states = {}, styles = {}, requests = {}, reducers = getEmptyObject, dispatchers = getEmptyObject,
-            store = createStore(combineReducers(reducers(states)), states)
+            app: {
+                $states = {},
+                $view = [],
+                $reducers = {},
+                $reducer = combineReducers($reducers),
+                $store = createStore($reducer, $states)
+            }
         } = props;
 
-        this.store = store;
-
-        // The state is probably still not needed here as it can be derived from the store.
-        proxyReactMethodsToSupportCustomDataProps(styles, store, dispatchers(states, store, requests));
-    }
-
-    componentWillMount() {
-        this.unsubscribe = this.store.subscribe(() => this.setState(this.store.getState()));
+        this.$store = $store;
+        this.$view = $view;
     }
 
     render() {
-        const {store, props: {children = []}} = this;
+        console.time('Render');
+        const element = <Provider store={this.$store}>{React.createElement(this.$view)}</Provider>;
+        console.timeEnd('Render');
 
-        return (<Provider store={store} children={React.Children.map(children, toClonedElement)}/>);
+        return element;
+    }
+
+    componentDidMount() {
+        this.unsubscribe = this.$store.subscribe(() => this.setState(this.$store.getState()));
     }
 
     componentWillUnmount() {
