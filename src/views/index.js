0604ea818033f5b122d5b6c4b1d8bbc9207b8f53
Tony Tahmouch <ttahmouch@gmail.com>
2019-11-12 21:15:58 -0500

    TT | Moving the entry point view declarations to the `views` folder.

---
+++ src/views/index.js
@@ -0,0 +1,27 @@
+import json_element from './json_element.json';
+import json_component_element from './json_component_element.json';
+import json_fragment_element from './json_fragment_element.json';
+import jsx_element from './jsx_element';
+import jsx_component_element from './jsx_component_element';
+import jsx_fragment_element from './jsx_fragment_element';
+import buttons from './buttons';
+import form from './form';
+import card from './card';
+import root from './root';
+import view from './view';
+
+export default {
+    json_element,
+    json_component_element,
+    json_fragment_element,
+    jsx_element,
+    jsx_component_element,
+    jsx_fragment_element,
+    buttons,
+    form,
+    card,
+    root,
+    view
+};
+
+export {view};
