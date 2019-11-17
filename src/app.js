2c9f6e18c0fe06c4ff228aa18101373cfc136783
Tony Tahmouch <ttahmouch@gmail.com>
2019-11-17 13:41:19 -0500

    TT | Move where `styles`, `actions`, `components`, and `views` get added to the store state to be at the level in which the `app` manifest declaration is created.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import {combineReducers, createStore} from 'redux';
-import $states from './states.js';
-import $styles from './styles.json';
+import states from './states/index.js';
+import $styles from './styles/index.js';
 import $actions from './actions/index.js';
-import $components from './components.js';
+import $components from './components/index.js';
 import $views, {view as $view} from './views/index.js';
 import {reducersFromStates} from './imperative/reducers.js';
 import {dispatchersForStore} from './imperative/dispatchers.js';
 
+// Namespace the non-states to allow them to spread flat on the state object and have reducers per item?
+// Consider shifting state down instead of spreading it on the root level.
+// Almost everything should probably live in the store state to allow for dynamic updates.
+const $states = {
+    ...states,
+    $styles,
+    $actions,
+    $components,
+    $views,
+};
 const $reducers = reducersFromStates($states);
 const $reducer = combineReducers($reducers);
 const $store = createStore($reducer, $states);
@@ -24,3 +34,16 @@ export default {
     $store,
     $dispatchers
 };
+
+console.group('Application State:');
+console.log($states);
+console.groupEnd();
+
+console.group('Application Imperative:');
+console.log({
+    $reducers,
+    $reducer,
+    $store,
+    $dispatchers
+});
+console.groupEnd();
